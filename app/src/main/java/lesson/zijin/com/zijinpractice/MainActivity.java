5cdb1977030c818e7c1ab55b338481ab09ccea26
ssn5730 <ssn5730@126.com>
2017-02-26 16:25:50 +0800

    add a TAG string

--- app/src/main/java/lesson/zijin/com/zijinpractice/MainActivity.java
+++ app/src/main/java/lesson/zijin/com/zijinpractice/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "MainActivity" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
