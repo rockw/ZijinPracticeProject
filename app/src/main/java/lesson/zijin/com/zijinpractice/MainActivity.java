764e1e0cfc3018eb75e0333d8925f44227cf445b
Your Name <you@example.com>
2017-03-05 15:10:23 +0800

    程磊140406116

--- app/src/main/java/lesson/zijin/com/zijinpractice/MainActivity.java
+++ app/src/main/java/lesson/zijin/com/zijinpractice/MainActivity.java
@@ -1,14 +1,24 @@
 package lesson.zijin.com.zijinpractice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private final static String TAG = "MainActivity" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1=(Button)findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,chenglei6116.class);
+                startActivity(intent);
+            }
+        });
     }
 }
