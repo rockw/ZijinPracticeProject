f83703e23527d9d04eb4dce17c23c89f7b543a24
liwei5730 <liwei5730@126.com>
2017-03-05 15:56:19 +0800

    add log

--- app/src/main/java/lesson/zijin/com/zijinpractice/MainActivity.java
+++ app/src/main/java/lesson/zijin/com/zijinpractice/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
         setContentView(R.layout.activity_main);
         Button button1=(Button)findViewById(R.id.button);
         button1.setOnClickListener(new View.OnClickListener() {
