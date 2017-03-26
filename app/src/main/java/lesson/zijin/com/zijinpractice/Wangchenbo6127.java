9bf02cae8967dce2b5cd579b3f3a223cbd2f7985
wangchenbo <437610464@qq.com>
2017-03-26 12:50:31 +0800

    add a chat ListView By wangchenbo 140406127

--- app/src/main/java/lesson/zijin/com/zijinpractice/Wangchenbo6127.java
+++ app/src/main/java/lesson/zijin/com/zijinpractice/Wangchenbo6127.java
@@ -1,13 +1,72 @@
 package lesson.zijin.com.zijinpractice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Wangchenbo6127 extends AppCompatActivity {
-
+    private ListView listView;
+    private wcbMsgAdapter adapter=null;
+    private ArrayList<wcbMsg> mylist=new ArrayList<wcbMsg>();
+    private EditText intput_text;
+    private Button btn_send;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题
+        initData();//初始化数据 将数据添加到容器中
         setContentView(R.layout.activity_wangchenbo6127);
+        //初始化控件
+        findView();
+
+        //将数据放入到适配器中
+        adapter=new wcbMsgAdapter(Wangchenbo6127.this,R.layout.wcb_chat_item,mylist);
+        listView.setAdapter(adapter);
+        //按钮的监听事件
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //
+                String content=intput_text.getText().toString();
+
+                //判空
+                if (TextUtils.isEmpty(content)){
+                    Toast.makeText(getApplication(),"你输入的内容为空",Toast.LENGTH_SHORT).show();
+                }else {
+                    //不为空
+                    wcbMsg msg=new wcbMsg(content,wcbMsg.TYPE_SEND);
+                    mylist.add(msg);
+                    //刷新消息
+                    adapter.notifyDataSetChanged();
+                    listView.setSelection(mylist.size());//将listview定位到最后一行
+                    intput_text.setText("");//清空
+                }
+            }
+        });
+    }
+
+
+
+    public void findView(){
+        listView=(ListView)findViewById(R.id.listview_chat);
+        intput_text=(EditText)findViewById(R.id.input_text);
+        btn_send=(Button)findViewById(R.id.btn_send);
+    }
+    public void initData(){
+
+        wcbMsg msg1=new wcbMsg("吃饭了吗？",wcbMsg.TYPE_RECEIVER);
+        mylist.add(msg1);
+        wcbMsg msg2=new wcbMsg("吃了！",wcbMsg.TYPE_SEND);
+        mylist.add(msg2);
+        wcbMsg msg3=new wcbMsg("什么时候出发？",wcbMsg.TYPE_RECEIVER);
+        mylist.add(msg3);
     }
 }
---
+++ app/src/main/java/lesson/zijin/com/zijinpractice/wcbMsgAdapter.java
@@ -0,0 +1,99 @@
+package lesson.zijin.com.zijinpractice;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 王陈博 on 2017/3/26.
+ */
+
+public class wcbMsgAdapter extends ArrayAdapter<wcbMsg>{
+    private Context context;
+    private  int resourceId;
+    private ArrayList<wcbMsg> list;
+
+    public wcbMsgAdapter(Context context,int resourceId,ArrayList<wcbMsg> list) {
+        //super();
+        super(context, resourceId, list);
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+
+    @Override
+    public wcbMsg getItem(int position) {
+        return super.getItem(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        wcbMsg msg = list.get(position);
+        return msg.getType();
+    }
+
+
+    @Override
+    public View getView(int position,View convertView,ViewGroup parent) {
+        //
+        ViewHolder viewHolder;
+       wcbMsg msg=getItem(position);
+        View view;
+        if (convertView==null){
+            //加载布局
+            view =  LayoutInflater.from(getContext()).inflate(R.layout.wcb_chat_item,null);
+            viewHolder = new ViewHolder();
+           // convertView= LayoutInflater.from(getContext()).inflate(resourceId,null);
+            //查找控件
+            viewHolder.left_layout=(LinearLayout)view.findViewById(R.id.left_layout);
+            viewHolder.right_layout=(LinearLayout)view.findViewById(R.id.right_layout);
+            viewHolder.tv_left=(TextView)view.findViewById(R.id.tv_left);
+            viewHolder.tv_right=(TextView)view.findViewById(R.id.tv_right);
+            view.setTag(viewHolder);
+        }else{
+            //直接获取
+            view = convertView;
+            viewHolder = (ViewHolder)view.getTag();
+        }
+
+        if (msg.getType()== wcbMsg.TYPE_RECEIVER)//如果它是接收消息 就放左边
+        {
+            //显示左边 隐藏右边
+            viewHolder.left_layout.setVisibility(View.VISIBLE);
+            viewHolder.right_layout.setVisibility(View.GONE);
+            //设置左边的内容
+            viewHolder.tv_left.setText(msg.getContent());
+        }else{
+            //显示右边 隐藏左边
+            viewHolder.left_layout.setVisibility(View.GONE);
+            viewHolder.right_layout.setVisibility(View.VISIBLE);
+            //设置右边的内容
+            viewHolder.tv_right.setText(msg.getContent());
+        }
+        return view;
+    }
+
+    static class ViewHolder{
+        static LinearLayout left_layout;
+        static LinearLayout right_layout;
+        static TextView tv_left;
+        static TextView tv_right;
+    }
+}
