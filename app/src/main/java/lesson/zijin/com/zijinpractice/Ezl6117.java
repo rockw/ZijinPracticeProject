b33cbbbb64d9bfd4e3c0e9fac6464e1717f6f40d
ezhaolong <1136166362@qq.com>
2017-04-09 16:25:05 +0800

    elz6117

--- app/src/main/java/lesson/zijin/com/zijinpractice/Ezl6117.java
+++ app/src/main/java/lesson/zijin/com/zijinpractice/Ezl6117.java
@@ -1,13 +1,63 @@
 package lesson.zijin.com.zijinpractice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class Ezl6117 extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import static lesson.zijin.com.zijinpractice.Msg_17.TYPE_RECEIVED;
+import static lesson.zijin.com.zijinpractice.Msg_17.TYPE_SENT;
+
+public class Ezl6117 extends Activity
+{
+    private ListView msgListView;
+
+    private EditText inputText;
+    private Button send;
+    private MsgAdapter_17 adapter;private List<Msg_17> msgList = new ArrayList<Msg_17>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_ezl6117);
+        initMsgs(); // 初始化消息数据
+        adapter = new MsgAdapter_17(Ezl6117.this, R.layout.msg_17, msgList);
+        inputText = (EditText) findViewById(R.id.input_text);
+        send = (Button) findViewById(R.id.send);
+        msgListView = (ListView) findViewById(R.id.msg_list_view);
+        msgListView.setAdapter(adapter);
+        send.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String content = inputText.getText().toString();
+                if (!"".equals(content))
+                {
+                    Msg_17 msg = new Msg_17(content, TYPE_SENT);
+                    msgList.add(msg);
+                    adapter.notifyDataSetChanged(); // 当有新消息时，刷新ListView中的显示
+                    msgListView.setSelection(msgList.size()); // 将ListView定位到最后一行
+                    inputText.setText(""); // 清空输入框中的内容
+                }
+            }
+        });
+    }
+    private void initMsgs()
+    {
+        Msg_17 msg1 = new Msg_17("你好，今天天气怎么样？", TYPE_RECEIVED);
+        msgList.add(msg1);
+        Msg_17 msg2 = new Msg_17("你好，今天天气很好。", TYPE_SENT);
+        msgList.add(msg2);
+        Msg_17 msg3 = new Msg_17("嗯，拜拜。", TYPE_RECEIVED);
+        msgList.add(msg3);
     }
 }
---
+++ app/src/main/java/lesson/zijin/com/zijinpractice/MsgAdapter_17.java
@@ -0,0 +1,62 @@
+package lesson.zijin.com.zijinpractice;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by admin on 2017/4/9.
+ */
+
+public class MsgAdapter_17 extends ArrayAdapter<Msg_17> {
+    private int resourceId;
+
+    public MsgAdapter_17(Context context, int textViewResourceId, List<Msg_17> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Msg_17 msg = getItem(position);
+        View view;
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, null);
+            viewHolder = new ViewHolder();
+            viewHolder.leftLayout = (LinearLayout) view.findViewById(R.id.left_layout);
+            viewHolder.rightLayout = (LinearLayout) view.findViewById(R.id.right_layout);
+            viewHolder.leftMsg = (TextView) view.findViewById(R.id.left_msg);
+            viewHolder.rightMsg = (TextView) view.findViewById(R.id.right_msg);
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        if (msg.getType() == Msg_17.TYPE_RECEIVED) {
+            // 如果是收到的消息，则显示左边的消息布局，将右边的消息布局隐藏
+            viewHolder.leftLayout.setVisibility(View.VISIBLE);
+            viewHolder.rightLayout.setVisibility(View.GONE);
+            viewHolder.leftMsg.setText(msg.getContent());
+        } else if (msg.getType() == Msg_17.TYPE_SENT) {
+            // 如果是发出的消息，则显示右边的消息布局，将左边的消息布局隐藏
+            viewHolder.rightLayout.setVisibility(View.VISIBLE);
+            viewHolder.leftLayout.setVisibility(View.GONE);
+            viewHolder.rightMsg.setText(msg.getContent());
+        }
+        return view;
+    }
+
+    class ViewHolder {
+        LinearLayout leftLayout;
+        LinearLayout rightLayout;
+        TextView leftMsg;
+        TextView rightMsg;
+    }
+}
