e5f6c3485ab386df2288e288c61cd6b15b9dda98
wangchenbo <437610464@qq.com>
2017-04-10 19:07:26 +0800

    add and change nine-patch photo by wangchenbo

--- app/src/main/java/lesson/zijin/com/zijinpractice/Wangchenbo6127.java
+++ app/src/main/java/lesson/zijin/com/zijinpractice/Wangchenbo6127.java
@@ -2,9 +2,7 @@ package lesson.zijin.com.zijinpractice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -21,12 +19,11 @@ public class Wangchenbo6127 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题
-        initData();//初始化数据 将数据添加到容器中
         setContentView(R.layout.activity_wangchenbo6127);
         //初始化控件
         findView();
-
+        //初始化数据 将数据添加到容器中
+        initData();
         //将数据放入到适配器中
         adapter=new WcbMsgAdapter(Wangchenbo6127.this,R.layout.wcb_chat_item,mylist);
         listView.setAdapter(adapter);
@@ -34,11 +31,9 @@ public class Wangchenbo6127 extends AppCompatActivity {
         btn_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //
                 String content=intput_text.getText().toString();
-
                 //判空
-                if (TextUtils.isEmpty(content)){
+                if ("".equals(content)){
                     Toast.makeText(getApplication(),"你输入的内容为空",Toast.LENGTH_SHORT).show();
                 }else {
                     //不为空
@@ -46,7 +41,7 @@ public class Wangchenbo6127 extends AppCompatActivity {
                     mylist.add(msg);
                     //刷新消息
                     adapter.notifyDataSetChanged();
-                    listView.setSelection(mylist.size());//将listview定位到最后一行
+                    listView.smoothScrollToPosition(mylist.size()-1);;//将listview定位到最后一行
                     intput_text.setText("");//清空
                 }
             }
@@ -54,14 +49,14 @@ public class Wangchenbo6127 extends AppCompatActivity {
     }
 
 
-
-    public void findView(){
+    //绑定控件
+    private void findView(){
         listView=(ListView)findViewById(R.id.listview_chat);
         intput_text=(EditText)findViewById(R.id.input_text);
         btn_send=(Button)findViewById(R.id.btn_send);
     }
-    public void initData(){
-
+    //初始化数据
+    private void initData(){
         WcbMsg msg1=new WcbMsg("吃饭了吗？",WcbMsg.TYPE_RECEIVER);
         mylist.add(msg1);
         WcbMsg msg2=new WcbMsg("吃了！",WcbMsg.TYPE_SEND);
--- app/src/main/java/lesson/zijin/com/zijinpractice/WcbMsgAdapter.java
+++ app/src/main/java/lesson/zijin/com/zijinpractice/WcbMsgAdapter.java
@@ -1,6 +1,8 @@
 package lesson.zijin.com.zijinpractice;
 
 import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,67 +10,37 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 王陈博 on 2017/3/26.
  */
 
 public class WcbMsgAdapter extends ArrayAdapter<WcbMsg>{
-    private Context context;
-    private  int resourceId;
-    private ArrayList<WcbMsg> list;
+    private  int resource;
 
-    public WcbMsgAdapter(Context context,int resourceId,ArrayList<WcbMsg> list) {
-        //super();
-        super(context, resourceId, list);
-        this.context = context;
-        this.list = list;
-    }
-
-    @Override
-    public int getCount() {
-        return list.size();
-    }
-
-
-    @Override
-    public WcbMsg getItem(int position) {
-        return super.getItem(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-
-    @Override
-    public int getItemViewType(int position) {
-        WcbMsg msg = list.get(position);
-        return msg.getType();
+    public WcbMsgAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<WcbMsg> objects) {
+        super(context, resource, objects);
+        this.resource = resource;
     }
 
 
     @Override
     public View getView(int position,View convertView,ViewGroup parent) {
-        //
-        ViewHolder viewHolder;
-       WcbMsg msg=getItem(position);
+        WcbMsg msg=getItem(position);
         View view;
+        ViewHolder viewHolder;
         if (convertView==null){
             //加载布局
-            view =  LayoutInflater.from(getContext()).inflate(R.layout.wcb_chat_item,null);
+            view =  LayoutInflater.from(getContext()).inflate(resource,parent,false);
             viewHolder = new ViewHolder();
-           // convertView= LayoutInflater.from(getContext()).inflate(resourceId,null);
             //查找控件
             viewHolder.left_layout=(LinearLayout)view.findViewById(R.id.left_layout);
             viewHolder.right_layout=(LinearLayout)view.findViewById(R.id.right_layout);
             viewHolder.tv_left=(TextView)view.findViewById(R.id.tv_left);
             viewHolder.tv_right=(TextView)view.findViewById(R.id.tv_right);
             view.setTag(viewHolder);
-        }else{
-            //直接获取
+        }else{ //直接获取
             view = convertView;
             viewHolder = (ViewHolder)view.getTag();
         }
@@ -90,10 +62,10 @@ public class WcbMsgAdapter extends ArrayAdapter<WcbMsg>{
         return view;
     }
 
-    static class ViewHolder{
-        static LinearLayout left_layout;
-        static LinearLayout right_layout;
-        static TextView tv_left;
-        static TextView tv_right;
+    private class ViewHolder{
+         LinearLayout left_layout;
+         LinearLayout right_layout;
+         TextView tv_left;
+         TextView tv_right;
     }
 }
