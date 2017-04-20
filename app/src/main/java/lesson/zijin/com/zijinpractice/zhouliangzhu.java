8af278d0d52ed281f3ab2c8631d4b6cb07f846cb
zhouliangzhu6114 <1041193858@qq.com>
2017-04-20 14:04:37 +0800

    add by zhouliangzhu6114

---
+++ app/src/main/java/lesson/zijin/com/zijinpractice/zhouliangzhu.java
@@ -0,0 +1,13 @@
+package lesson.zijin.com.zijinpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class zhouliangzhu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhouliangzhu);
+    }
+}
