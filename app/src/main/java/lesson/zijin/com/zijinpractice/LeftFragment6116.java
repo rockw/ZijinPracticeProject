536bfc8702d885a25a4f44b310fc21bd1585cfc2
Your Name <you@example.com>
2017-05-14 16:54:32 +0800

    chenglei6116

---
+++ app/src/main/java/lesson/zijin/com/zijinpractice/LeftFragment6116.java
@@ -0,0 +1,15 @@
+package lesson.zijin.com.zijinpractice;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LeftFragment6116 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_left_6116,container,false);
+        return view;
+    }
+}
