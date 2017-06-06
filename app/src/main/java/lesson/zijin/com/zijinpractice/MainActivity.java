34104cd339eec48cac5fa56b621e6714924a17a7
lingweishidabiao <911745068@qq.com>
2017-06-06 15:32:43 +0800

    lw6123

--- app/src/main/java/lesson/zijin/com/zijinpractice/MainActivity.java
+++ app/src/main/java/lesson/zijin/com/zijinpractice/MainActivity.java
@@ -181,6 +181,14 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(intent);
                     }
                 });
+                 Button lw = (Button) findViewById(R.id.lw);
+        qnn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, lw6123.class);
+                startActivity(intent);
+            }
+        });
         Button wjy = (Button) findViewById(R.id.wjy);
         wjy.setOnClickListener(new View.OnClickListener() {
             @Override
